6296c2ba1ca5875d0ff3da3790f8c6efa4fe8439
Leschenko_Rostyslav <rostishkaleschenko2006@gmail.com>
2024-11-10 17:13:04 +0200

    TryRead

--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Lab
+{
+    class MyExceptions
+    {
+        private Exception ex1 = new Exception("Such file doesnt exist");
+        private Exception ex2 = new OverflowException();
+        private Exception ex3 = new Exception("digits arent int");
+        public void TryRead(string name)
+        {
+            try
+            {
+                IDK.Read(name);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+
+    class IDK
+    {
+        static public void Read(string name)
+        {
+            StreamReader sr = new StreamReader(name);
+        }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
